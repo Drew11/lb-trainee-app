cbef6adfd4c20dff40333d2a245943f9ac7d7953
Drew11 <drew.naumenko@gmail.com>
2021-03-24 10:33:12 +0200

    test server

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const data = require('./data');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/stats', function (req, res) {
     res.send(JSON.stringify(data));
